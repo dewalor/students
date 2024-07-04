5b550b9796d1d0d18fcdf818c46b96d68b4a124e
dewalor <dewalor@proton.me>
2024-07-04 19:07:56 +0800

    list students api

--- main.go
+++ main.go
@@ -1,17 +1,19 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+
+	"students/models"
+	"students/controllers"
+
 )
 
 func main() {
 	r := gin.Default()
 
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
-	})
+	models.ConnectDatabase()
+
+	r.GET("/students", controllers.FindStudents)
 
 	r.Run()
 }
